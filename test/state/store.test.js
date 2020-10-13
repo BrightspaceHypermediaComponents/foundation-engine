a4282e283c25617a3e6b998c01cb1874d3b763ad
Alexander Kobets <alexander.kobets@D2L.com>
2020-10-12 22:36:17 -0400

    more tests and questions

--- test/state/store.test.js
+++ test/state/store.test.js
@@ -26,6 +26,8 @@ describe('state/store.js', () => {
 		expect(state).to.be.instanceOf(HypermediaState);
 		expect(state.entityId, 'entityId').to.be.equal(id);
 		expect(state.token.value, 'token').to.be.equal(token);
+		const storedState = window.D2L.SirenSdk.StateStore.get(id, token);
+		expect(storedState, 'stored state').to.be.equal(state); // BUG, returns false instead of state object for state that is created with TOKEN_COOKIE
 	}
 
 	beforeEach(() => {
@@ -45,6 +47,13 @@ describe('state/store.js', () => {
              framework/lit/HypermediaStateMixin.js,
              framework/lit/HypermediaResult.js,
              state/sirenComponents/SirenLink.js
+
+            this._states map has key as a token value and map as a value. value map has keys of state objects and value of { refCount: number } ?
+            Questions:
+              Why duplicate calls to _initContainer ?
+                 window.D2L.SirenSdk.StateStore.makeNewState and window.D2L.SirenSdk.StateStore.add are both called from stateFactory
+                and both methods call _initContainer.
+
         */
 
 		it('should retun new state when created with unique entityId and token', async() => {
@@ -142,5 +151,62 @@ describe('state/store.js', () => {
 			}
 		});
 
+		it('Design Question: when creating state it uses whatever letter case of entityId, and then it uses lowercase entityId to check if state exists', async() => {
+			const state = await createTestState('ID', 'token'); // entity created with upper case entityId
+			const state2 = await createTestState('id', 'token'); // then is matched to existing entity
+			expect(state).to.be.equal(state2);
+			verifyState(state2, 'ID', 'token');
+			verifyState(state, 'ID', 'token');
+
+			const state3 = await createTestState('id2', 'token'); // entity created with lower case entityId
+			const state4 = await createTestState('ID2', 'token'); // then is matched texisting
+			expect(state3).to.be.equal(state4);
+			verifyState(state3, 'id2', 'token');
+			verifyState(state4, 'id2', 'token');
+		});
+
 	});
+
+	describe.skip('stateFactoryByRawSirenEntity', () => {
+		/*
+          Testing notes:
+            Used in state/sirenComponents/SirenSubEntity.js
+          Questions:
+            Unclear code path when getEntityIdFromSirenEntity returns value which undefined or null or empty string.
+            In this case state is still created, however it is not added to the StateStore
+            Also it is not clear why there is call state.onServerResponse(rawEntity);
+        */
+	});
+
+	describe.skip('performAction', () => {
+		/*
+          Testing notes:
+            Used in state/sirenComponents/SirenAction.js
+          Questions:
+            Unclear why there is a call to await action.refreshToken() without actually using returned refreshed token.
+            in StateStore class method there is a:
+                const state = this.get(entity.getLink('self').href, action.token);
+                state.onServerResponse(entity);
+            What if this.get returns undefined ?
+
+        */
+	});
+
+	describe.skip('dispose', () => {
+		/*
+          Testing notes:
+            Used in framework/lit/HypermediaStateMixin.js
+          Questions:
+            Should it belong to the store? When tracing what the method does, there is no a sing call that is related to the StateStore
+
+        */
+	});
+
+	describe.skip('get', () => {
+		/*
+        testing notes:
+          Code is mind twister. It reads like a JS quizz question: function whatItReturns(){ return true && 'string' && 5;}
+        */
+	});
+
 });
