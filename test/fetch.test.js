6a2506638eec0d7a49c0f577fd7e6b34e9c83d29
Maya Jones <Maya.Jones@D2L.com>
2020-11-27 12:07:13 -0500

    Starting test writing and update stateStore

--- test/fetch.test.js
+++ test/fetch.test.js
@@ -5,10 +5,28 @@ class FetchableObject extends Fetchable(Object) {}
 
 describe('Fetch', () => {
 	let fetchableInstance;
+	//let sandbox;
+
 	beforeEach(() => {
+		/*
+		sandbox = sinon.sandbox.create();
+
+		sandbox.stub(window.d2lfetch, 'fetch').callsFake(() => {
+
+			return Promise.resolve({
+				ok: false,
+				status: 404
+			}
+			);
+		});
+		*/
 		fetchableInstance = new FetchableObject('testHref', 'token');
 	});
-
+	/*
+	afterEach(() => {
+		sandbox.restore();
+	});
+	*/
 	it('starts and completes the status', () => {
 		expect(fetchableInstance.href).to.equal('testHref');
 	});
