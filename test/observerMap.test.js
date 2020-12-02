80a80fc56c0d93d8d7fc0c0ae8c033afbca299cb
Ten Bradley <69812595+tenbradley@users.noreply.github.com>
2020-12-02 15:19:14 -0500

    Add merge method to ObserverMap (#27)
    * added merge method to ObserverMap and used it in SirenLink and SirenSubEntity
    
    * updated observerMap and wrote tests
    
    * feat: FETCH, Actions and MORE! (#21)
    
    * base fetch
    
    * Actions work
    Everything works!
    
    * entityId to entityID
    
    * Fix some assumptions.
    
    * name change
    file not found fix.
    
    * Now the tests are fixed.
    
    * Starting test writing and update stateStore
    
    * Remove StateStore
    
    * StateStore
    
    * Remove fetch.test.js Another story will add this.
    
    * Tests added for SirenSubEntities (#25)
    
    * base fetch
    
    * more fetching
    
    * Connecting everything
    
    * base fetch
    
    * Actions work
    Everything works!
    
    * entityId to entityID
    
    * Fix some assumptions.
    
    * name change
    file not found fix.
    
    * Now the tests are fixed.
    
    * Starting test writing and update stateStore
    
    * subEntities tests
    
    * updated tests
    
    * sirenSubEntities tests
    
    * Update SirenLink.js
    
    * Delete sirenObserverFactory.js
    
    * moved testing data
    
    * cleaning of files
    
    * rename StateStore.js
    
    Co-authored-by: Maya Jones <Maya.Jones@D2L.com>
    
    * recommented some broken tests
    
    Co-authored-by: Ten Bradley <69812595+tenbradley@users.noreply.github.com>
    Co-authored-by: ten.bradley@d2l.com <Ten.Bradley@D2L.com>
    
    * changed merge parameter name
    
    * Oops setting value twice. (#29)
    
    Co-authored-by: Maya Jones <m6jones@users.noreply.github.com>

--- test/observerMap.test.js
+++ test/observerMap.test.js
@@ -64,13 +64,140 @@ describe('ObserverMap', () => {
 		};
 		const property = 'property';
 		const method = undefined;
-		const value = 'href';
 		const newValue = 'newHref';
 
-		observerMap.add(observer, property, method, value);
+		observerMap.add(observer, property, method);
 		observerMap.setProperty(newValue);
 
 		assert(observer[property] === newValue);
 	});
 
+	describe('should merge observers', () => {
+		let observerMap2;
+		beforeEach(() => {
+			observerMap2 = new ObserverMap();
+		});
+
+		it('should merge two observers, only the one merged to is altered', () => {
+			const observer1 = {
+				'bar': 'foo'
+			};
+			const observer2 = {
+				'abc': 'efg'
+			};
+			const property = 'property';
+			const value1 = 'hello';
+			const value2 = 'goodbye';
+
+			observerMap.add(observer1, property);
+			observerMap.setProperty(value1);
+
+			observerMap2.add(observer2, property);
+			observerMap2.setProperty(value2);
+
+			observerMap.merge(observerMap2);
+
+			assert.equal(observerMap._observers.size, 2, 'should have two observers after merge');
+			assert.equal(observerMap._observers.get(observer1), property, 'should have original observer unmodified');
+			assert.equal(observerMap._observers.get(observer2), property, 'should have merged observer added');
+			assert.equal(observer1[property], value1, 'first observer should map to original property');
+			assert.equal(observer2[property], value1, 'second observer should map to new property');
+
+			assert.equal(observerMap2._observers.size, 1, 'observerMap2 should be unaltered after merge');
+		});
+
+		it('should be unaltered after merging map with same entry', () => {
+			const observer1 = {
+				'bar': 'foo'
+			};
+
+			const property = 'property';
+			const value1 = 'hello';
+
+			observerMap.add(observer1, property);
+			observerMap.setProperty(value1);
+
+			observerMap2.add(observer1, property);
+
+			observerMap.merge(observerMap2);
+
+			assert.equal(observerMap._observers.size, 1, 'should have two observers after merge');
+			assert.equal(observerMap._observers.get(observer1), property, 'should have original observer unmodified');
+			assert.equal(observer1[property], value1, 'first observer should map to original property');
+
+			assert.equal(observerMap2._observers.size, 1, 'observerMap2 should be unaltered after merge');
+		});
+
+		it('should merge two large observerMaps', () => {
+			const observer1 = {
+				'bar': 'foo'
+			};
+			const observer2 = {
+				'abc': '123'
+			};
+			const observer3 = {
+				'xyz': '789'
+			};
+			const observer4 = {
+				'name': 'foo'
+			};
+
+			const property = 'property';
+			const link = 'link';
+			const value1 = 'hello';
+			const value2 = 'goodbye';
+
+			observerMap.add(observer1, property);
+			observerMap.add(observer2, link);
+			observerMap.setProperty(value1);
+
+			observerMap2.add(observer3, property);
+			observerMap2.add(observer4, link);
+			observerMap2.setProperty(value2);
+
+			observerMap.merge(observerMap2);
+
+			assert.equal(observerMap._observers.size, 4, 'should have two observers after merge');
+			assert.equal(observerMap._observers.get(observer1), property, 'observer1 should be property');
+			assert.equal(observerMap._observers.get(observer2), link, 'observer2 should be link');
+			assert.equal(observerMap._observers.get(observer3), property, 'observer3 should be property');
+			assert.equal(observerMap._observers.get(observer4), link, 'observer4 should be link');
+			assert.equal(observer1[property], value1, 'observer1 should map to original property');
+			assert.equal(observer2[link], value1, 'observer2 should map to original property');
+			assert.equal(observer3[property], value1, 'observer3 should map to updated');
+			assert.equal(observer4[link], value1, 'observer1 should map to updated property');
+
+			assert.equal(observerMap2._observers.size, 2, 'observerMap2 should be unaltered after merge');
+		});
+
+		it('should merge two observers and apply method', () => {
+			const observer1 = {
+				'bar': 'foo'
+			};
+			const observer2 = {
+				'abc': 'efg'
+			};
+			const property = 'property';
+			const value1 = 'hello';
+			const value2 = 'goodbye';
+			const method = (val) => `${val}${val}`;
+			assert.isFunction(method);
+
+			observerMap.add(observer1, property);
+			observerMap.setProperty(value1);
+
+			observerMap2.add(observer2, property, method);
+			observerMap2.setProperty(value2);
+
+			observerMap.merge(observerMap2);
+
+			assert.equal(observerMap._observers.size, 2, 'should have two observers after merge');
+			assert.equal(observerMap._observers.get(observer1), property, 'should have original observer unmodified');
+			assert.equal(observerMap._observers.get(observer2), property, 'should have merged observer added');
+			assert.equal(observer1[property], value1, 'first observer should map to original property');
+			assert.equal(observer2[property], `${value1}${value1}`, 'second observer should map to new mapped property');
+
+			assert.equal(observerMap2._observers.size, 1, 'observerMap2 should be unaltered after merge');
+		});
+	});
 });
