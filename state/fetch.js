4412e7ffa542a8b012747254ce4263142655f5cc
Maya Jones <Maya.Jones@D2L.com>
2020-12-08 21:40:44 -0500

    Add comments!

--- state/fetch.js
+++ state/fetch.js
@@ -2,6 +2,16 @@ import 'd2l-fetch/d2l-fetch.js';
 
 const d2lfetch = window.d2lfetch;
 
+/**
+ * This method sends request to the server. It returns the results to fetchable.onServerResponse and the returned promise.
+ * This method also handles cachePrimer header from the server with links with the rel, https://api.brightspace.com/rels/cache-primer.
+ * A list of the links that need to be primed are sent to fetchable.handleCachePriming method.
+ * @param {Fetchabe} fetchable Is an object that inherients from the class fetchable. This is the object that will be fetched.
+ * @param {boolean} bypassCache Default is `false`. If bypassCache is true then it will cancel any pending request
+ *                              and send a new request to the server with no-cache headers.
+ * @returns {Promise} This promise will resolve to a json object, or null if the request is canceled.
+ * 					  If there was an error in fetching then this promised is rejected with an error message.
+ */
 export function fetch(fetchable, bypassCache = false) {
 	if (fetchable.fetchStatus.pending) {
 		if (!bypassCache) {
@@ -21,6 +31,13 @@ export function fetch(fetchable, bypassCache = false) {
 	return responsePromise;
 }
 
+/**
+ * This method is a helper for fetch. It handles the actual sending of the request.
+ * @param {Fetchabe} fetchable Is an object that inherients from the class fetchable. This is the object that will be fetched.
+ * @param {boolean} bypassCache Default is `false`. If bypassCache is true then it will cancel any pending request
+ *                              and send a new request to the server with no-cache headers.
+ * @returns {Promise} This promise will resolve when fetchable is fetched
+ */
 async function performServerFetch(fetchable, bypassCache) {
 	await fetchable.refreshToken();
 
@@ -44,7 +61,11 @@ async function performServerFetch(fetchable, bypassCache) {
 		fetchable.fetchStatus.done(null, err);
 	}
 }
-
+/**
+ * This method constructs a array of links that we need to start handling!
+ * @param {Response} response A successful response from the server. One with response.ok = true.
+ * @returns {Array} An array of strings that represents links we need to handle.
+ */
 function cachePrimingList(response) {
 	const linkHeaderValues = response.headers && response.headers.get('Link');
 	if (!linkHeaderValues) {
