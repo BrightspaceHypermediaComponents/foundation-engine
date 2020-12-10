f0a46dca8384cb0620e15a0b43fcc67cc3a8127f
ten.bradley@d2l.com <Ten.Bradley@D2L.com>
2020-12-10 11:36:53 -0500

    added set action tests

--- test/observable/sirenSummonAction.test.js
+++ test/observable/sirenSummonAction.test.js
@@ -11,11 +11,37 @@ describe('SirenSummonAction', () => {
 		assert.instanceOf(summonAction._routes, Map, 'routes initializes to empty map');
 	});
 
-	it('set action', () => {
-		const summonAction = new SirenSummonAction({ id: 'foo', token: 'abc', prime: true });
-		summonAction.action = { has: true, summon: () => 'result' };
+	describe('set action', () => {
+		it('should set action to given object', () => {
+			const summonAction = new SirenSummonAction({ id: 'foo', token: 'abc', prime: true });
+			summonAction.action = { has: true, summon: () => 'result' };
 
-		assert.isTrue(summonAction.action.has, 'action has a function');
-		assert.equal(summonAction.action.summon(), 'result', 'calling summon returns its result');
+			assert.isTrue(summonAction.action.has, 'action has a function');
+			assert.equal(summonAction.action.summon(), 'result', 'calling summon returns its result');
+		});
+
+		it('has is false, should set action to defaultSummon', () => {
+			const summonAction = new SirenSummonAction({ id: 'foo', token: 'abc', prime: true });
+			summonAction.action = { has: false, summon: () => 'result' };
+
+			assert.isFalse(summonAction.action.has, 'action does not have summon');
+			assert.isUndefined(summonAction.action.summon(), 'result from summon should be undefined');
+		});
+
+		it('summon is not function, should set action to defaultSummon', () => {
+			const summonAction = new SirenSummonAction({ id: 'foo', token: 'abc', prime: true });
+			summonAction.action = { has: true, summon: 'summon' };
+
+			assert.isFalse(summonAction.action.has, 'action does not have summon');
+			assert.isUndefined(summonAction.action.summon(), 'result from summon should be undefined');
+		});
+
+		it('has is false and summon is not function, should set action to defaultSummon', () => {
+			const summonAction = new SirenSummonAction({ id: 'foo', token: 'abc', prime: true });
+			summonAction.action = { has: false, summon: 'summon' };
+
+			assert.isFalse(summonAction.action.has, 'action does not have summon');
+			assert.isUndefined(summonAction.action.summon(), 'result from summon should be undefined');
+		});
 	});
 });
