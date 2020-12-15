8009293471769121c0450e4a6dbf8ef781682095
Zina Ramirez <instinctualart@gmail.com>
2020-12-14 22:09:15 -0500

    Added SirenFacade and unit tests

--- test/data/observable/entities.js
+++ test/data/observable/entities.js
@@ -1,66 +1,66 @@
 export const subEntitiesTests = { fooEntity:`{
-  "entities": [{"rel": ["foo"], "href": "www.abc.com"}],
-  "links": [{
-	"rel": ["self"],
-	"href": "http://example.com"
-  }],
-  "href": "hello"
+	"entities": [{"rel": ["foo"], "href": "www.abc.com"}],
+	"links": [{
+		"rel": ["self"],
+		"href": "http://example.com"
+	}],
+	"href": "hello"
 }`,
 barEntity: `{
-  "entities": [{"rel": ["bar"], "href": "www.abc.com"}],
-  "links": [{
-	"rel": ["self"],
-	"href": "http://example.com"
-  }],
-  "href": "hello"
+	"entities": [{"rel": ["bar"], "href": "www.abc.com"}],
+	"links": [{
+		"rel": ["self"],
+		"href": "http://example.com"
+	}],
+	"href": "hello"
 }`,
 multipleSubEntities: `{
 	"entities": [{"rel": ["foo"], "href": "www.def.com"},{"rel": ["foo"], "href": "www.xyz.com"}],
 	"links": [{
-	  "rel": ["self"],
-	  "href": "http://example.com"
+		"rel": ["self"],
+		"href": "http://example.com"
 	}],
 	"href": "hello"
-  }`
+	}`
 };
 
 export const linkTests = { fooRel: `{
 	"links": [{
-	  "rel": ["foo"],
-	  "href": "http://example.com"
+		"rel": ["foo"],
+		"href": "http://example.com"
 	}]
-  }`, barRel: `{
-	"links": [{
-	  "rel": ["bar"],
-	  "href": "http://example.com"
-	}]
-  }`
+	}`, barRel: `{
+		"links": [{
+			"rel": ["bar"],
+			"href": "http://example.com"
+		}]
+	}`
 };
 
 export const subEntityTests = { entityWithHref: `{
 	"entities": [{
-        "rel": ["foo"],
-        "links": [{
-            "rel": ["self"],
-            "href": "www.subentity.com"
+		"rel": ["foo"],
+		"links": [{
+			"rel": ["self"],
+			"href": "www.subentity.com"
 		}],
 		"href": "www.foo.com"
-    }]
-  }`, entityWithoutHref:`{
+	}]
+	}`, entityWithoutHref:`{
 	"entities": [{
-        "rel": ["foo"],
-        "links": [{
-            "rel": ["self"],
-            "href": "www.subentity.com"
+		"rel": ["foo"],
+		"links": [{
+			"rel": ["self"],
+			"href": "www.subentity.com"
 		}]
-    }]
-  }`, barEntity: `{
+	}]
+	}`, barEntity: `{
 	"entities": [{
-        "rel": ["bar"],
-        "links": [{
-            "rel": ["self"],
-            "href": "www.subentity.com"
+		"rel": ["bar"],
+		"links": [{
+			"rel": ["self"],
+			"href": "www.subentity.com"
 		}]
-    }]
-  }`
+	}]
+	}`
 };
