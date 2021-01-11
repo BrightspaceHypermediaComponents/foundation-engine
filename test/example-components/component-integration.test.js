5e02607dba92ca218937099b35f0c0fa55d46189
Maya Jones <m6jones@users.noreply.github.com>
2021-01-11 15:30:19 -0500

    feat: Routed updates (#69)
    * Wrote integration test that is broken for developing
    
    * lint
    
    * IT works!
    
    * ONly nope! ALL TESTs
    
    Co-authored-by: Zina Ramirez <instinctualart@gmail.com>

--- test/example-components/component-integration.test.js
+++ test/example-components/component-integration.test.js
@@ -1,4 +1,5 @@
 import './summon-action-components.js';
+import './subentities-components.js';
 import { aTimeout, expect, fixture, waitUntil } from '@open-wc/testing';
 import { default as fetchMock } from 'fetch-mock/esm/client.js';
 import { html } from 'lit-html';
@@ -95,5 +96,99 @@ describe('Component integration', () => {
 
 		await aTimeout(400);
 		expect(element._hasAction('nestedSummon'), 'has the action').to.be.true;
+	});
+
+	describe('SubEntities', () => {
+		beforeEach(() => {
+			fetchMock.reset();
+		});
+
+		it('gets the subEntities', async() => {
+			const selfHref = 'https://entity';
+			const entity = {
+				entities: [{
+					properties: { prop1: 'foo', prop2: 'bar' },
+					rel: ['item']
+				}],
+				links: [{ rel: ['self'], href: selfHref }]
+			};
+			const mock = fetchMock.mock(selfHref, JSON.stringify(entity));
+			const element = await fixture(html`<subentities-component href="${selfHref}" token="someToken"></subentities-component>`);
+			await waitUntil(() => element._loaded === true);
+			expect(mock.called(selfHref)).to.be.true;
+			expect(element.items).to.exist;
+			expect(element.items).to.be.an('array');
+			expect(element.items).to.have.lengthOf(1);
+			expect(element.items[0].properties.prop1).to.be.equal('foo');
+		});
+
+		it('gets the routed subEntities', async() => {
+			const selfHref = 'https://entity';
+			const linkedHref = 'https://entity/linked';
+			const entity = {
+				links: [{ rel: ['self'], href: selfHref }, { rel: ['linked'], href: linkedHref }]
+			};
+			const linkedEntity = {
+				entities: [{
+					properties: { prop1: 'foo', prop2: 'bar' },
+					rel: ['item']
+				}]
+			};
+			const mock = fetchMock
+				.mock(selfHref, JSON.stringify(entity))
+				.mock(linkedHref, JSON.stringify(linkedEntity), { delay: 500 });
+			const element = await fixture(html`<routed-subentities-component href="${selfHref}" token="someToken"></routed-subentities-component>`);
+			await waitUntil(() => element._loaded === true);
+			expect(mock.called(selfHref)).to.be.true;
+			expect(mock.called(linkedHref)).to.be.true;
+			expect(element.items).to.exist;
+			expect(element.items).to.be.an('array');
+			expect(element.items).to.have.lengthOf(1);
+			expect(element.items[0].properties.prop1).to.be.equal('foo');
+		});
+
+		it('updates the state from the component', async() => {
+			const entityHref = 'https://entity';
+			const routedHref = 'https://entity-routed';
+			const entity = {
+				entities: [{
+					properties: { prop1: 'foo', prop2: 'bar' },
+					rel: ['item']
+				}],
+				links: [{ rel: ['self'], href: entityHref }]
+			};
+			const entityWithRouting = {
+				links: [{ rel: ['self'], href: routedHref }, { rel: ['linked'], href: entityHref }]
+			};
+			const mock = fetchMock
+				.mock(entityHref, JSON.stringify(entity))
+				.mock(routedHref, JSON.stringify(entityWithRouting));
+			const element = await fixture(html`<subentities-component href="${entityHref}" token="someToken"></subentities-component>`);
+			const elementWithRouting = await fixture(html`<routed-subentities-component href="${routedHref}" token="someToken"></routed-subentities-component>`);
+			await waitUntil(() => element._loaded === true);
+			expect(mock.called(entityHref)).to.be.true;
+			expect(mock.called(routedHref)).to.be.true;
+
+			expect(element.items).to.exist;
+			expect(element.items).to.be.an('array');
+			expect(element.items).to.have.lengthOf(1);
+			expect(elementWithRouting.items).to.exist;
+			expect(elementWithRouting.items).to.be.an('array');
+			expect(elementWithRouting.items).to.have.lengthOf(1);
+
+			// update the state from the first component
+			element.updateItems([{
+				properties: { newProp: 'cake' }
+			}]);
+			expect(element.items[0].properties.newProp).to.be.equal('cake');
+			expect(elementWithRouting.items[0].properties.newProp, 'second observing component sees change').to.be.equal('cake');
+			// update the state from the second component
+			elementWithRouting.updateItems([{
+				properties: { newProp: 'cupcake' }
+			}]);
+			expect(elementWithRouting.items[0].properties.newProp, 'routed component sees change').to.be.equal('cupcake');
+			expect(element.items[0].properties.newProp, 'component directly observing sees change').to.be.equal('cupcake');
+
+		});
 	});
 });
